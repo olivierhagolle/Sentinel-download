b79459b977ecd0ebf82b16bb7d236a76b7e3db05
Gert Sterenborg <gertsterenborg@gmail.com>
2016-12-09 10:44:03 +0100

    New naming convention scihub FIXES #19

--- Sentinel_download.py
+++ Sentinel_download.py
@@ -204,6 +204,7 @@ if geom=='point':
 elif geom=='rectangle':
     if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
         query_geom='footprint:\\"Intersects(POLYGON(({lonmin} {latmin}, {lonmax} {latmin}, {lonmax} {latmax}, {lonmin} {latmax},{lonmin} {latmin})))\\"'.format(latmin=options.latmin,latmax=options.latmax,lonmin=options.lonmin,lonmax=options.lonmax)
+        print query_geom
     else :
         query_geom='footprint:"Intersects(POLYGON(({lonmin} {latmin}, {lonmax} {latmin}, {lonmax} {latmax}, {lonmin} {latmax},{lonmin} {latmin})))"'.format(latmin=options.latmin,latmax=options.latmax,lonmin=options.lonmin,lonmax=options.lonmax)
     
@@ -278,7 +279,10 @@ for i in range(len(request_list)):
 
         #test if product is within the requested time period
         if options.sentinel.startswith("S2"):
-            date_prod=filename.split('_')[7][1:9]
+            if len(filename.split("_")) == 7:
+                date_prod=filename.split('_')[-1][:8]
+            else:
+                date_prod = filename.split('_')[7][1:9]
         elif  options.sentinel.startswith("S1"):
             date_prod=filename.split('_')[5][0:8]
         else :
